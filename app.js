40d1ae9b6ce1b6d1db7fff420d98aca8edb6cdec
naman99lalit <naman.lalit@gmail.com>
2020-03-16 19:53:12 +0530

    Added: Project Folder

---
+++ app.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const Nexmo = require('nexmo');
+const socketio = require('socket.io');
+
+// Init Nexmo
+const nexmo = new Nexmo({
+    apiKey: '609db9c0',
+    apiSecret: 'GwVIa62D9DH1B7xB'
+},{debug:true});
+
+
+// Init app
+const app = express();
+
+// Template engine setup
+app.set('view engine', 'html');
+app.engine('html', ejs.renderFile);
+
+// Public folder setup
+app.use(express.static(__dirname+ '/public'));
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+// Index Route
+app.get('/',(req,res)=>{
+    res.render('index');
+});
+
+// Catch form submit
+app.post('/',(req,res)=>{
+    // res.send(req.body);
+    // console.log(req.body);
+    const number = req.body.number;
+    const text = req.body.text;
+    nexmo.message.sendSms(
+        '917988998027', number, text,{ type: 'unicode'},
+        (err,responseData)=>{
+            if(err){
+                console.log(err);
+            }else{
+                console.dir(responseData);
+                // Get data from the response 
+                const data = {
+                    id: responseData.messages[0]['message-id'],
+                    number: responseData.messages[0]['to']
+                }
+                // Emit to the client
+                io.emit('smsStatus',data);
+            }
+        }
+    );
+});
+
+// Define Port
+const port = 3000;
+
+// Start Server
+const server = app.listen(port, ()=> console.log(`Server started on port ${port}`));
+
+// Connect to Socket.io
+const io = socketio(server);
+io.on('connection',(socket)=>{
+    console.log('Coonected');
+    io.on('disconnect',()=>{
+        console.log('Disconnected');
+    })
+})
